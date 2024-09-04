10f21502415b0c9453d8fe1fcf56bb35392665aa
Ranjeet <ranjeetkumar9264@gmail.com>
2024-09-04 22:49:13 +0530

    tic tac toe game

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Main from './Components/Main';
+import Nav from './Components/Nav';
+import "./app.css";
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
